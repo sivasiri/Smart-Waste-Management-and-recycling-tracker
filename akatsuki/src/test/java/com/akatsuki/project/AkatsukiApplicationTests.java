edb80e4dcd2c357e1daba1fbf7da7545aa9bec4b
Killua007899 <siva.k.siriki@gmail.com>
2025-02-27 23:23:24 -0600

    Created the project | First commit

---
+++ akatsuki/src/test/java/com/akatsuki/project/AkatsukiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akatsuki.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkatsukiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
